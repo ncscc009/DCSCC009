58514df0d19b4a31a2c78b0c8664d4c12d3c34eb
bhavya241 <sahithyareddy2004@gmail.com>
2025-07-03 13:31:13 +0530

    commiting the code

---
+++ prgm1.java
@@ -0,0 +1,7 @@
+// This is a simple Java program to print "Hello, World!" to the console.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println() is used to print a line of text to the console.
+        System.out.println("Hello, World!");
+    }
+}
